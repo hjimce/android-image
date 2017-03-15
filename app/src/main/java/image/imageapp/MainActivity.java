c30f146edeae757ba88c7084593fb699b9e7b711
hjimce <hjimce.huang@avatarworks.com>
2017-03-15 07:51:27 -0400

    network

--- app/src/main/java/image/imageapp/MainActivity.java
+++ app/src/main/java/image/imageapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.opengl.GLSurfaceView;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,9 +15,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+//GPUimage库导入
 import jp.co.cyberagent.android.gpuimage.GPUImage;
-import jp.co.cyberagent.android.gpuimage.GPUImageSepiaFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageSobelEdgeDetection;
+//renderscript导入
+import android.renderscript.RenderScript;
+import network.CNNdroid;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -82,19 +86,43 @@ public class MainActivity extends AppCompatActivity {
             //mGPUImage.setGLSurfaceView((GLSurfaceView) findViewById(R.id.imageView));//加入这句，程序会崩溃
             mGPUImage.setImage(scaled); //设置gpu要处理的图片
             mGPUImage.setFilter(new GPUImageSobelEdgeDetection());//设置滤波方法
-            d = mGPUImage.getBitmapWithFilterApplied();//进行gpu图像处理，获取处理后的图片
+            Bitmap resultimage = mGPUImage.getBitmapWithFilterApplied();//进行gpu图像处理，获取处理后的图片
+
+
+            try {
+                // 1) Create a Renderscript object
+                RenderScript myRenderScript = RenderScript.create(this);
+
+                // 2) Construct a CNNdroid object
+                //	  and provide NetFile location address.
+                String NetFile = "/storage/emulated/0/tmp/alexnet/AlexNet_def.txt";
+                CNNdroid myCNN = new CNNdroid(myRenderScript, NetFile);
+
+                // 3) Prepare your input to the network.
+                //		(The input can be single or batch of images.)
+                //float[][][]  inputSingle = new float[3][227][227];
+
+                // 4) Call the Compute function of the CNNdroid library
+                //    and get the result of the CNN execution as an Object
+                //	  when the computation is finished.
+                //Object output = myCNN.compute(inputSingle);
+
+
+
+            } catch (Exception e) {
+                //TextView tv = (TextView) findViewById(R.id.sample_text);
+                //tv.setText(stringFromJNI(picturePath));
+                //tv.setText(e.toString());
+                e.printStackTrace();
+            }
 
 
 
 
 
 
+            imageView.setImageBitmap(resultimage);//设置view控件中的显示内容
 
-            imageView.setImageBitmap(d);//设置view控件中的显示内容
-
-
-            TextView tv = (TextView) findViewById(R.id.sample_text);
-            tv.setText(stringFromJNI(picturePath));
 
 
         }
